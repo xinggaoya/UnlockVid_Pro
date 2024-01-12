7c43d0efc324d6fe45c8ec64839cbb619bc427ee
XingGao <1032227283@qq.com>
2024-01-13 00:21:34 +0800

    Revert "增加剧集显示动态切换"
    This reverts commit 9014dc6a10749fcb499b0cdf3bb1febbd604284b.

--- src/api/system/videoInfo.ts
+++
@@ -1,9 +0,0 @@
-import service from "@/api/request";
-
-export const getVideoInfo = (data: any) => {
-    return service({
-        url: '/api/info',
-        method: 'get',
-        params: data
-    })
-}
